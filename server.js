3fc5b3f2d9b9f9d8f59f509dcaa3551ce8fe5b52
Tiia Toivonen <tiiatoiv@metropolia.fi>
2021-04-15 13:23:12 +0300

    jelastic stuff working now?

--- server.js
+++ server.js
@@ -6,27 +6,47 @@ import resolvers from './resolvers/index.js';
 import express from 'express';
 import connectDB from './database/db.js'
 import dotenv from 'dotenv';
+import {checkAuth} from "./passport/authenticate.js";
+import helmet from 'helmet';
+import cors from 'cors';
+import remote from './host/remote.js'
 
 dotenv.config();
 
 (async () => {
     try {
         const con = await connectDB();
-        if(con){
+        if (con) {
             console.log('connection to db succesful');
         }
         const server = new ApolloServer({
             typeDefs: schemas,
             resolvers: resolvers,
+            context: async ({req, res}) => {
+                try {
+                    const user = await checkAuth(req, res);
+                    return {req, res, user};
+                } catch (error) {
+                    console.log('context error:', error);
+                }
+            }
         });
         const app = express();
+        app.use(helmet({
+            ieNoOpen: false
+        }));
+        app.use(cors());
+        server.applyMiddleware({app, path: '/graphql'});
+        console.log(process.env.NODE_ENV);
+        process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+        if (process.env.NODE_ENV === 'production') {
+            remote(app, 3000);
+        } else {
+            console.log('localhost');
+            const { default: localhost } = await import('./host/localhost.js');
+            localhost(app, 8000, 3000);
+        }
 
-        server.applyMiddleware({app});
-
-        app.listen({port: 3000}, () =>
-            console.log(
-                `🚀 Server ready at http://localhost:3000${server.graphqlPath}`),
-        );
     } catch (e) {
         console.log('server error: ' + e.message);
     }
